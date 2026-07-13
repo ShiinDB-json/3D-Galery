7ea8e6c7e4c6abb8392b7dae0d7b5fd31924c9c7
ShiinDB-json <kyykntk@gmail.com>
2026-07-13 00:45:55 +0000

    Optimize build: chunk Three.js and raise size warning limit

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 800,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three'],
+        },
+      },
+    },
+  },
 })
